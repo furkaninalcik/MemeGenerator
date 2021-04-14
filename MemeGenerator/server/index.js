eb6e05cbd78043c3cee38bbad6d249372949cc2a
furkaninalcik <m.furkaninalcik@gmail.com>
2021-04-14 21:06:54 +0000

    added the google storage connection, images are saved to a storage bucket with public access

--- MemeGenerator/server/index.js
+++ MemeGenerator/server/index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const multer = require("multer");
+const Multer = require("multer");
 
 const PORT = process.env.PORT || 3001;
 
@@ -7,7 +7,9 @@ const app = express();
 
 
 const path = require('path');
+const {format} = require('util');
 
+// resource for the code : https://cloud.google.com/appengine/docs/standard/nodejs/using-cloud-storage
 
 
 // Have Node serve the files for our built React app
@@ -77,33 +79,83 @@ app.get('/images', (req, res) => {
 
 // image upload
 
-const storageInfo = multer.diskStorage({
-    destination: function (req, file, callback)  {
-        callback(null, 'server/images/')
-    },
-    filename: function (req, file, callback) {
-        const { originalname } = file;
-
-        callback(null, originalname);
-    }
-
-})
+//localStorageInfo is not used!
+//const localStorageInfo = multer.diskStorage({
+//    destination: function (req, file, callback)  {
+//        callback(null, "/tmp/meme-generator-images")
+//    },
+//    filename: function (req, file, callback) {
+//        const { originalname } = file;
+//
+//        callback(null, originalname);
+//    }
+//
+//})
 
 
 //const upload = multer({ dest: "server/images/" });
-const upload = multer({ storage: storageInfo });
+//const upload = multer({ storage: storageInfo });
 
 //app.use(express.static('public'))
 //app.use(express.static('images'))
-app.use('/images', express.static('images')); 
+//app.use('/images', express.static('images')); 
 
-app.post("/upload", upload.single("baseImage"),
-    (req, res) => {
-        console.log(req.file)
-        res.status(200).contentType("text/plain").end(req.file.originalname + "Image uploaded!!!");
-        
-    }
-);
+//app.post("/upload", upload.single("baseImage"),
+//    (req, res) => {
+//        console.log(req.file)
+//        res.status(200).contentType("text/plain").end(req.file.originalname + "Image uploaded!!!");
+//        
+//    }
+//);
+
+const {Storage} = require('@google-cloud/storage');
+
+// Instantiate a storage client
+const storage = new Storage();
+
+
+// Multer is required to process file uploads and make them available via
+// req.files.
+const multer = Multer({
+  storage: Multer.memoryStorage(),
+  limits: {
+    fileSize: 7 * 1024 * 1024, // max size is 7mb
+  },
+});
+
+const bucket = storage.bucket(process.env.GCLOUD_STORAGE_BUCKET);
+// A bucket is a container for objects (files).
+
+
+app.post('/upload', multer.single('baseImage'), (req, res, next) => {
+  if (!req.file) {
+    res.status(400).send('No image uploaded.');
+    return;
+  }
+
+  // Create a new blob in the bucket and upload the file data.
+  const blob = bucket.file(req.file.originalname);
+  const blobStream = blob.createWriteStream({
+    resumable: false,
+  });
+
+  blobStream.on('error', err => {
+    next(err);
+  });
+
+  blobStream.on('finish', () => {
+
+    console.log("UPLOAD FINISHED")
+
+    // The public URL can be used to directly access the file via HTTP.
+    const publicUrl = format(
+      `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+    );
+    res.status(200).send(publicUrl);
+  });
+
+  blobStream.end(req.file.buffer);
+});
 
 
 
